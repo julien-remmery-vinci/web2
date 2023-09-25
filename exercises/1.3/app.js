bc53dd8bcbc1e5ac7a089055cc5e6f7208efec90
julien-remmery-vinci <julien.remmery@student.vinci.be>
2023-09-25 14:50:50 +0200

    1.3 : API : create & REST client

---
+++ exercises/1.3/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index.js');
+var usersRouter = require('./routes/users.js');
+var filmsRouter = require('./routes/films.js');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/films', filmsRouter);
+
+module.exports = app;
