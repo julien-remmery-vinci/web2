cf85fa2e4671b441d91d96a2c0643c9e5a4bfbf4
julien-remmery-vinci <julien.remmery@student.vinci.be>
2023-10-09 14:56:38 +0200

    2.2 : HMI : dom interaction

--- exercises/2.2/index.js
+++ exercises/2.2/index.js
@@ -1,12 +1,29 @@
 const btn1 = document.querySelector('#button');
 const nbClicks = document.querySelector('#nbClicks');
 const message = document.querySelector('#message');
+const resetbtn = document.querySelector('#reset');
 
 let clicks = 0;
 
 btn1.addEventListener('click', () => {
     clicks++;
-    nbClicks.innerHTML = "Nombre de clics : "+clicks;
-    if(clicks>=5 && clicks <=9) message.innerHTML = "Bravo, bel échauffement !";
-    else if(clicks>=10) message.innerHTML = "Vous êtes passé maître en l'art du clic !";
+    display();
 });
+
+resetbtn.addEventListener('click', () => {
+    clicks = 0;
+    display();
+});
+
+function display(){
+    nbClicks.innerHTML = "Nombre de clics : "+clicks;
+    if(clicks == 0) {
+        message.innerHTML = "";
+        resetbtn.hidden = true;
+    }
+    else if(clicks>=5 && clicks <=9) {
+        message.innerHTML = "Bravo, bel échauffement !";
+        resetbtn.hidden = false;
+    }
+    else if(clicks>=10) message.innerHTML = "Vous êtes passé maître en l'art du clic !";
+}
